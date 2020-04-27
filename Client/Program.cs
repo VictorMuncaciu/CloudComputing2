fcbeb8ea201d7e039d19e2d67bf4242df6932338
VictorMuncaciu <muncaciuvictor@gmail.com>
2020-04-28 02:57:21 +0300

    Adding project

---
+++ Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using Grpc.Core;
+using ZodiacFinderGrpc;
+
+namespace ZodiacClient
+{
+    class Program
+    {
+        private static bool IsDateValid(string date)
+        {
+            var dateFormats = new[] {"dd/MM/yyyy","dd/MM/yyy","dd/MM/yy","dd/MM/y"};
+            Console.WriteLine(date);
+            return DateTime.TryParseExact(
+                date,
+                dateFormats,
+                DateTimeFormatInfo.InvariantInfo,
+                DateTimeStyles.None, 
+                out _);
+        }
+        
+        private static BirthDateRequest PromptBirthday()
+        {
+            Console.Write("Date of birth: ");
+            var dateOfBirthStr = Console.ReadLine();
+            if (dateOfBirthStr==null || !IsDateValid(dateOfBirthStr))
+            {
+                Console.WriteLine("Invalid date!");
+                System.Environment.Exit(1);
+            }
+
+            var fields = dateOfBirthStr.Split('/');
+
+            var bdr=new BirthDateRequest();
+
+            bdr.Day = Int32.Parse(fields[0]);
+            bdr.Mo = Int32.Parse(fields[1]);
+            bdr.Year = Int32.Parse(fields[2]);
+            
+            return bdr;
+        }
+        
+        public static void Main(string[] args)
+        {
+            Channel channel = new Channel("localhost:88888", ChannelCredentials.Insecure);
+
+            var client = new ZodiacService.ZodiacServiceClient(channel);
+
+            var birthRequest = PromptBirthday();
+
+            var reply = client.findZodie(birthRequest);
+            Console.WriteLine("Season: " + reply.Season + " || Sign: "+ reply.Sign);
+
+            Console.ReadKey();
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+using Grpc.Core;
+using ZodiacFinderGrpc;
+
+namespace ZodiacFinder
+{
+    class ZodiacFinderImpl : ZodiacService.ZodiacServiceBase
+    {
+        
+        private static string ZodiacFile = ".."+Path.DirectorySeparatorChar+".."+Path.DirectorySeparatorChar+".."+Path.DirectorySeparatorChar+"zodiac.txt";
+
+        public override Task<ZodiacReply> findZodie(BirthDateRequest request, ServerCallContext context)
+        {
+            Console.WriteLine("Request: "+request.Day+"/"+request.Mo+"/"+request.Year);
+
+            Task<ZodiacReply> reply=null;
+
+            try
+            {
+                reply = Task.FromResult(FindSignForDate(request));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+            return reply;
+        }
+
+        //Citeste fisierul de zodii
+        public static SignRange[] ReadSignFile()
+        {
+            var fileContent = System.IO.File.ReadAllText(ZodiacFile);
+
+            //array cu toate liniile din fisierul de zodii
+            var lines = fileContent.Split("\n");
+
+            //array cu obiecte care reprezinta intervalul fiecarei zodii
+            var signRanges = new SignRange[12];
+            for (var i=0; i<lines.Length; i++)
+            {
+                //campurile prin care intervalul zodiei este reprezentat
+                var fields = lines[i].Split(";");
+                var start = fields[0];
+                var end = fields[1];
+
+                //indexul zodiei din fiseriul .proto
+                var signIndex = Int32.Parse(fields[2]);
+
+                var startDay = Int32.Parse(start.Split("/")[0]);
+                var startMonth = Int32.Parse(start.Split("/")[1]);
+                var endDay = Int32.Parse(end.Split("/")[0]);
+                var endMonth = Int32.Parse(end.Split("/")[1]);
+
+                //creem obiect cu toate proprietetiile unui interval de zodie
+                var signRange=new SignRange{StartDay = startDay, StartMonth = startMonth, EndDay = endDay, EndMonth = endMonth, SignIndex = signIndex};
+                
+                signRanges[i] = signRange;
+            }
+
+            return signRanges;
+        }
+
+        private ZodiacReply FindSignForDate(BirthDateRequest birthDate)
+        {
+            var day = birthDate.Day;
+            var month = birthDate.Mo;
+            var year = birthDate.Year;
+            
+            var season = Seasons.Spring;
+            var sign = ZodiacSign.Aquarius;
+
+            var signRanges = ReadSignFile();
+
+            int signIndex = -1;
+            foreach (var signRange in signRanges)
+            {
+                if ((month == signRange.StartMonth && day>=signRange.StartDay) || (month == signRange.EndMonth && day<=signRange.EndDay) )
+                {
+                    signIndex = signRange.SignIndex;
+                    break;
+                }
+            }
+
+            if (signIndex > -1)
+            {
+                if (month == 1 || month == 2 || month==12)
+                {
+                    season = Seasons.Winter;
+                }else if (month == 3 || month == 4 || month == 5)
+                {
+                    season = Seasons.Spring;
+                }else if (month == 6 || month == 7 || month == 8)
+                {
+                    season = Seasons.Summer;
+                }else if (month == 9 || month == 10 || month == 11)
+                {
+                    season = Seasons.Fall;
+                }
+                
+                return new ZodiacReply() {Season = season, Sign = (ZodiacSign)signIndex};
+            }
+            else
+            {
+                Console.WriteLine("Could not find sign");
+                return null;
+            }
+        }
+    }
+
+    class Program
+    {
+        const int Port = 88888;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { ZodiacService.BindService(new ZodiacFinderImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            
+            Console.WriteLine("Server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+            
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
